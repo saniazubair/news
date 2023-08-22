a7ba3fec2a5e6ebabee518faf5f25bc3d6d25154
saniazubair <mughalayesha648@gmail.com>
2023-08-21 18:35:30 -0700

    Initial commit

--- articles/models.py
+++ articles/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
 # Create your models here.
 from django.db import models
 
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    content = RichTextField()
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
