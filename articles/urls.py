f03055bc6f374b7abd7737185e83551bffd6d2f2
saniazubair <mughalayesha648@gmail.com>
2023-08-23 18:01:06 -0700

    initial commit

--- articles/urls.py
+++ articles/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.article_list, name='article_list'),
+    path('article/<int:id>/', views.article_detail, name='article_detail'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -6,3 +6,6 @@ def article_list(request):
     articles = Article.objects.all()
     return render(request, 'article_list.html', {'articles': articles})
 
+def article_detail(request, id):
+    article = Article.objects.get(id=id)
+    return render(request, 'article_detail.html', {'article': article})
